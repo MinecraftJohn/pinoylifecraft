f3a1a75ffcbe49ad548d1ce1b5a1de3debdc1c66
John Napoles <84890923+MinecraftJohn@users.noreply.github.com>
2021-10-06 20:07:45 +0800

    Added iFrame on AboutTrailer

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var navList = document.getElementById("navList")
 var mobilenavLogo = document.getElementById("mobilenavLogo")
 var mobilenavMenuBackground = document.getElementById("mobilenavMenuBackground")
 var aboutTrailerVideoBG = document.getElementById("aboutTrailerVideoBG")
+var aboutTrailerVideo = document.getElementById("aboutTrailerVideo")
 
 // PRE LOADER or SPLASH SCREEN
 window.addEventListener("load", function () {
@@ -35,8 +36,10 @@ function mobilenavMenuFadein () {
 
 // TRAILER (PLC SERVER)
 function trailerVideoOpen () {
+    aboutTrailerVideo.setAttribute("src", "https://www.youtube.com/embed/ac5s6a5ZbWM?autoplay=1")
     aboutTrailerVideoBG.style.display = "block"
 }
 function trailerVideoClose () {
+    aboutTrailerVideo.setAttribute("src", "https://www.youtube.com/embed/ac5s6a5ZbWM?autoplay=0")
     aboutTrailerVideoBG.style.display = "none"
 }
